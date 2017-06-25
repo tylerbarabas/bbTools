25bfc437b1be30d84decb3ac15a2a10964f120bc
Tyler Barabas <tylerb@d2.com>
2017-06-25 02:57:37 -0700

    added styled-components and styled-classnames, widened the viewport

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
-import './PageLayout.scss'
+import styled from 'styled-components'
 
 export const PageLayout = ({ children }) => (
-  <div className='container text-center'>
+  <div className='text-center'>
     <h1>bbTools</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
+    <IndexLink to='/'>Home</IndexLink>
     {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    <Link to='/counter'>Counter</Link>
     {' · '}
-    <Link to='/lipsyncer' activeClassName='page-layout__nav-item--active'>Lip Syncer</Link>
-    <div className='page-layout__viewport'>
+    <Link to='/lipsyncer'>Lip Syncer</Link>
+    <div>
       {children}
     </div>
   </div>
 )
+
 PageLayout.propTypes = {
   children: PropTypes.node,
 }
--- src/routes/LipSyncer/components/LipSyncer.js
+++ src/routes/LipSyncer/components/LipSyncer.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: red;
+  height: 30px;
+  width: 30px;
+`;
 
 export const LipSyncer = ({ lipsyncer, increment, doubleAsync, triple }) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>LipSyncer: {lipsyncer}</h2>
-    <button className='btn btn-primary' onClick={increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-secondary' onClick={doubleAsync}>
-      Double (Async)
-    </button>
-    <button className='btn btn-secondary' onClick={triple}>
-     Triple
-    </button>
-  </div>
+  <Container>
+     
+  </Container>
 )
+
 LipSyncer.propTypes = {
   lipsyncer: PropTypes.number.isRequired,
   increment: PropTypes.func.isRequired,
