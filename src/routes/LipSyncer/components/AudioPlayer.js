39526631cd61f3db2f01699ead64c4bdd2761232
Tyler Barabas <tylerb@d2.com>
2017-07-03 11:15:44 -0700

    bound the playback speed to the input

--- src/routes/LipSyncer/components/AudioPlayer.js
+++ src/routes/LipSyncer/components/AudioPlayer.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 class AudioPlayer extends React.Component {
 
   changed(e) {
-//    this.props.changePlaybackRate(e.target.value);
+    this.props.changePlaybackRate(e.target.value);
     document.getElementById('audio-player').playbackRate = e.target.value;
   }
 
@@ -19,7 +19,7 @@ class AudioPlayer extends React.Component {
           <source src="kingoftheworld.wav" type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
-        <input type="number" step="0.25" min="0.5" max="4" value="1" onChange={this.changed.bind(this)} style={{width: '50px', display: 'inline-block', verticalAlign: 'top'}} />
+        <input type="number" step="0.25" min="0.5" max="4" value={data.playbackRate} onChange={this.changed.bind(this)} style={{width: '50px', display: 'inline-block', verticalAlign: 'top'}} />
       </div>
     )
   }
